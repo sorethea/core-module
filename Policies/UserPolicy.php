b69415030ee41495040ec89c482d568230441b1b
Sorethea OK <sorethea@gmail.com>
2023-01-26 15:47:12 +0700

    lam-dev

--- Policies/UserPolicy.php
+++ Policies/UserPolicy.php
@@ -20,4 +20,21 @@ class UserPolicy
     public function view(User $user, User $model): bool{
         return $user->can("users.view");
     }
+
+    public function create(User $user):bool{
+        return $user->can('users.create');
+    }
+
+    public function update(User $user):bool{
+        return $user->can('users.update');
+    }
+    public function delete(User $user):bool{
+        return $user->can('users.delete');
+    }
+    public function restore(User $user):bool{
+        return $user->can('users.restore');
+    }
+    public function forceDelete(User $user):bool{
+        return $user->can('users.forceDelete');
+    }
 }
