affaaa01950bfda6a7f200db24202a9192ef5e23
Sorethea OK <sorethea@gmail.com>
2023-01-27 10:21:39 +0700

    lam-dev

--- Database/Seeders/CoreDatabaseSeeder.php
+++ Database/Seeders/CoreDatabaseSeeder.php
@@ -16,6 +16,6 @@ class CoreDatabaseSeeder extends Seeder
     {
         Model::unguard();
 
-        // $this->call("OthersTableSeeder");
+        $this->call(PermissionTableSeeder::class);
     }
 }
--- Providers/AuthServiceProvider.php
+++ Providers/AuthServiceProvider.php
@@ -4,12 +4,19 @@ namespace Modules\Core\Providers;
 
 use App\Models\User;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+use Modules\Core\Filament\Resources\PermissionResource;
+use Modules\Core\Filament\Resources\RoleResource;
+use Modules\Core\Policies\PermissionPolicy;
 use Modules\Core\Policies\UserPolicy;
+use Spatie\Permission\Models\Permission;
+use Spatie\Permission\Models\Role;
 
 class AuthServiceProvider extends ServiceProvider
 {
     protected $policies =[
         User::class=>UserPolicy::class,
+        Role::class=>RoleResource::class,
+        Permission::class=>PermissionPolicy::class,
     ];
     public function register()
     {
