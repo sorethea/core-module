c1277e6f6bbeb169f4ff3b3b22b06c49e05d9c4c
Sorethea OK <sorethea@gmail.com>
2023-01-27 10:22:40 +0700

    lam-dev

---
+++ Database/Seeders/PermissionTableSeeder.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Modules\Core\Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Database\Eloquent\Model;
+use Spatie\Permission\Models\Permission;
+use Spatie\Permission\Models\Role;
+
+class PermissionTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Model::unguard();
+
+        $role = Role::findOrCreate("Admin");
+        $module = "core";
+        $models =[
+            "users",
+            "roles",
+            "permissions",
+        ];
+        foreach ($models as $model){
+            Permission::findOrCreate($model.'.viewAny');
+            Permission::findOrCreate($model.'.view');
+            Permission::findOrCreate($model.'.create');
+            Permission::findOrCreate($model.'.update');
+            Permission::findOrCreate($model.'.delete');
+            Permission::findOrCreate($model.'.restore');
+            Permission::findOrCreate($model.'.forceDelete');
+            $manage = Permission::findOrCreate($model.'.manage');
+            $role->givePermissionTo($manage);
+        }
+        $user = User::create([
+            "name"=>"Administrator",
+            "email"=>"admin",
+            "password"=>"12345678"
+        ]);
+        $user->assignRole($role);
+
+    }
+}
