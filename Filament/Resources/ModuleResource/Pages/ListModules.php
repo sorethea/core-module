36483797add0ba0511e128f0da6101569da0158f
Sorethea OK <sorethea@gmail.com>
2023-01-30 13:24:58 +0700

    lam-dev

--- Filament/Resources/ModuleResource/Pages/ListModules.php
+++ Filament/Resources/ModuleResource/Pages/ListModules.php
@@ -5,15 +5,38 @@ namespace Modules\Core\Filament\Resources\ModuleResource\Pages;
 use Modules\Core\Filament\Resources\ModuleResource;
 use Filament\Pages\Actions;
 use Filament\Resources\Pages\ListRecords;
+use Modules\Core\Models\Module;
 
 class ListModules extends ListRecords
 {
     protected static string $resource = ModuleResource::class;
+
+    public function mount(): void
+    {
+        parent::mount();
+        $this->loadModules();
+    }
 
     protected function getActions(): array
     {
         return [
             //Actions\CreateAction::make(),
         ];
+    }
+
+    public function loadModules(): void{
+        $modules = \Module::all();
+        foreach ($modules as $module){
+            $installed = false;
+            if($module->getName()=="Core"){
+                $installed = true;
+            }
+            Module::query()->firstOrCreate([
+                'name'=>$module->getName(),
+                'enabled'=>$module->isEnabled(),
+                'installed'=>$installed,
+            ]);
+        }
+        $this->render();
     }
 }
