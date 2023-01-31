500572fbe1998f10deb054dcc64c848da1fbd133
Sorethea OK <sorethea@gmail.com>
2023-01-31 09:40:26 +0700

    lam-dev

--- Classes/Core.php
+++ Classes/Core.php
@@ -7,7 +7,7 @@ class Core
     public function getType($moduleName):string{
         $type = 'module';
         $module = \Module::find($moduleName);
-        dd($module);
+        dd($module->getPath());
         return $type;
     }
 }
