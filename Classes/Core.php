e90855ad4e0df9aef9f44fd3669ef6baa6ab97b4
Sorethea OK <sorethea@gmail.com>
2023-01-31 09:49:20 +0700

    lam-dev

--- Classes/Core.php
+++ Classes/Core.php
@@ -5,9 +5,8 @@ namespace Modules\Core\Classes;
 class Core
 {
     public function getType($moduleName):string{
-        $type = 'module';
         $module = \Module::find($moduleName);
-        dd($json = json_decode(file_get_contents($module->getPath()."/module.json"), true));
-        return $type;
+        $json = json_decode(file_get_contents($module->getPath()."/module.json"), true);
+        return $json['type']??'module';
     }
 }
