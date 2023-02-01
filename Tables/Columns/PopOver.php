b432769a90db5b6e1c7dbc122abdb3cf171ebc64
Sorethea OK <sorethea@gmail.com>
2023-02-01 10:07:36 +0700

    lam-dev

--- Tables/Columns/PopOver.php
+++ Tables/Columns/PopOver.php
@@ -7,4 +7,9 @@ use Filament\Tables\Columns\Column;
 class PopOver extends Column
 {
     protected string $view = 'core::tables.columns.pop-over';
+
+    public function getState()
+    {
+        return "Hello";
+    }
 }
