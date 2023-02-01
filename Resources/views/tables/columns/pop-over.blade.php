8e6a52c9dcab30448c5788a7d01fef64d5c8cc00
Sorethea OK <sorethea@gmail.com>
2023-02-01 10:03:43 +0700

    lam-dev

--- Resources/views/tables/columns/pop-over.blade.php
+++ Resources/views/tables/columns/pop-over.blade.php
@@ -1,14 +1,3 @@
 <div>
     {{ $getState() }}
-    <x-filament-popover::preview :view="'core::popover-table'"
-                                 :viewData="[]"
-                                 :allowHTML="true"
-                                 :arrow="false"
-                                 :theme="'light'"
-                                 :interactive="true"
-                                 :placement="'bottom'"
-                                 :animation="'shift-away'"
-    >
-        Show tooltip!
-    </x-filament-popover::preview>
 </div>
