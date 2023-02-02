92f30380d2a7fff141e7b7c0916f618c0b3c0046
Sorethea OK <sorethea@gmail.com>
2023-02-02 14:53:27 +0700

    Phone relation manager

--- Filament/Resources/UserResource/RelationManagers/PhonesRelationManager.php
+++ Filament/Resources/UserResource/RelationManagers/PhonesRelationManager.php
@@ -36,8 +36,8 @@ class PhonesRelationManager extends RelationManager
             ->columns([
                 Tables\Columns\TextColumn::make('phone_number')->searchable(),
                 Tables\Columns\TextColumn::make("remark"),
-                Tables\Columns\BooleanColumn::make('is_default'),
-                //Tables\Columns\ToggleColumn::make('is_default'),
+                //Tables\Columns\BooleanColumn::make('is_default'),
+                Tables\Columns\ToggleColumn::make('is_default'),
             ])
             ->filters([
                 //
