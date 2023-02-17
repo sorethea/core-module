024758bb1e1490415d52c803dd2082e219c535b0
Sorethea OK <sorethea@gmail.com>
2023-02-17 13:49:36 +0700

    lam-dev

--- Classes/CoreModule.php
+++ Classes/CoreModule.php
@@ -9,6 +9,12 @@ use Nwidart\Modules\Module;
 
 class CoreModule extends Module
 {
+    protected $name;
+
+    public function __construct(Container $app, string $name, $path)
+    {
+        parent::__construct($app, $name, $path);
+    }
 
     public function install(): void
     {
